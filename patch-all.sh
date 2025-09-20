165d10eb230b132ba89febc064b02a8faec5c318
taoky <me@taoky.moe>
2025-09-20 17:21:14 +0800

    Parallelize

--- patch-all.sh
+++ patch-all.sh
@@ -4,8 +4,14 @@ COMMIT=9b0f1436e455d902de067a2501422e5dc71ad16b
 
 mkdir -p output/
 
-for i in src/"$COMMIT"/NotoSansCJK-*.ttc; do
-    bname=$(basename "$i")
-    ./patch.py "$i" "output/$bname"
-    echo "$bname" done
-done
+# single-thread version:
+# for i in src/"$COMMIT"/NotoSansCJK-*.ttc; do
+#     bname=$(basename "$i")
+#     ./patch.py "$i" "output/$bname"
+#     echo "$bname" done
+# done
+
+ls src/"$COMMIT"/NotoSansCJK-*.ttc | parallel '
+    bname=$(basename {});
+    ./patch.py {} output/$bname && echo $bname done
+'
